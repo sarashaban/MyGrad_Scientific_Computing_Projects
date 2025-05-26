1872e37d7c03380072ddc35a5455fbb9927d8bb3
Maliheh Shaban Tameh, Ph.D. <malihe.shaban@gmail.com>
2025-05-26 02:06:27 -0700

    Update main.cpp

--- Sparse_Matrix/src/main.cpp
+++ Sparse_Matrix/src/main.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 int main() {
-    SparseMatrix A(3, 3), B(3, 3), C(3, 3);
-    A.setData({0, 0, 2, 2}, {0, 1}, {5.0, -7.0});
-    B.setData({0, 1, 2, 4}, {1, 1, 0, 2}, {3.0, -6.0, 4.0, -3.0});
-    C.setData({0, 0, 0, 1}, {1}, {8.0});
+    SparseMatrix A(3, 3),B(3, 3),C(3, 3);
+    A.setData({0, 0, 2, 2},{0, 1},{5.0, -7.0});
+    B.setData({0, 1, 2, 4},{1, 1, 0, 2},{3.0, -6.0, 4.0, -3.0});
+    C.setData({0, 0, 0, 1},{1},{8.0});
 
-    SparseMatrix D = A + B + C;
+    SparseMatrix D = A+B+C;
     D.print();
     D.saveToFile("output_matrix.txt");
 
